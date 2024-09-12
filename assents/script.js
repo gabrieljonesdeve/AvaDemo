d413ec3e5be4577f9c97daaf8822881c1b5b222d
Gabriel Jones <gabriel.jones@mail.urp.gjdev.it>
2024-09-12 10:58:18 +0200

    add

---
+++ assents/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('/base/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-placeholder').innerHTML = data;
+        })
+        .catch(error => console.error('Errore nel caricamento dell\'header:', error));
+});

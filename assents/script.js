4dede0fba0f2fc4525c67b5f0b4f9a79c6cfd5e8
Gabriel Jones <gabriel.jones@mail.urp.gjdev.it>
2024-09-12 11:02:29 +0200

    Update script.js

--- assents/script.js
+++ assents/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('base/header.html')
+    fetch('https://gabrieljonesdeve.github.io/AvaDemo/assents/base/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;

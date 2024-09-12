757a3708fa301d32aad4fa620e8944581ac35eed
Gabriel Jones <gabriel.jones@mail.urp.gjdev.it>
2024-09-12 11:00:16 +0200

    add base

--- assents/script.js
+++ assents/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/base/header.html')
+    fetch('base/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
